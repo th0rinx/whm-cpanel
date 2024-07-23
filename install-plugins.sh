0cf92f63e4117dadac760d4f82f7cba99c3a43d6
Guille <87372312+th0rinx@users.noreply.github.com>
2024-07-23 10:33:58 -0300

    Create install-plugins.sh

---
+++ install-plugins.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+
+wget -N http://files.softaculous.com/install.sh
+chmod 755 install.sh
+./install.sh
+
+# Actualizar el sistema
+echo "Actualizando el sistema..."
+sudo apt update && sudo apt upgrade -y
+
+# Instalar CSF
+echo "Instalando CSF..."
+sudo apt install -y libwww-perl liblwp-protocol-https-perl
+cd /usr/src
+sudo wget https://download.configserver.com/csf.tgz
+sudo tar -xzf csf.tgz
+cd csf
+sudo bash install.sh
+
+# Configuración básica de CSF
+echo "Configurando CSF..."
+sudo perl /usr/local/csf/bin/csftest.pl
+sudo systemctl start csf
+sudo systemctl enable csf
+
+
+# Instalar el plugin de Acronis para WHM
+echo "Instalando el plugin de Acronis para WHM..."
+sh <(curl -L https://download.acronis.com/ci/cpanel/stable/install_acronis_cpanel.sh || wget -O - https://download.acronis.com/ci/cpanel/stable/install_acronis_cpanel.sh)
+
+
+
+
+
+echo "Plugins Insalados correctamente"
