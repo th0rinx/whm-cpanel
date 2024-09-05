46b69c714887d857427c6f8efe4620afcbe88e6d
Guille <87372312+th0rinx@users.noreply.github.com>
2024-09-05 12:16:16 -0300

    Update install-plugins.sh
    cambio de codigo en la instalacion de acronis

--- install-plugins.sh
+++ install-plugins.sh
@@ -29,7 +29,7 @@ cd /usr/src
 sudo wget https://download.configserver.com/csf.tgz
 sudo tar -xzf csf.tgz
 cd csf
-sudo bash install.sh
+sudo bash install.sh -y
 
 # Configuración básica de CSF
 echo "Configurando CSF..."
@@ -39,6 +39,8 @@ sudo systemctl enable csf
 
 # Instalar el plugin de Acronis para WHM
 echo "Instalando el plugin de Acronis para WHM..."
-curl -sL https://download.acronis.com/ci/cpanel/stable/install_acronis_cpanel.sh | sudo bash || wget -O - https://download.acronis.com/ci/cpanel/stable/install_acronis_cpanel.sh | sudo bash
+
+
+wget https://download.acronis.com/ci/cpanel/current/deb/acronis-backup-cpanel_1.9.0.773_amd64.deb && dpkg -i acronis-backup-cpanel_1.9.0.773_amd64.deb
 
 echo "Plugins instalados correctamente"
